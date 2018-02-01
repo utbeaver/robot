7768213ff2f3308f78844fd7fa2d936843b383de
utbeaver <peilin.song@gmail.com>
2018-01-31 23:56:01 -0500

    axes appears

--- invkinematics.py
+++ invkinematics.py
@@ -59,6 +59,7 @@ class link:
         self.update()
         self.calllist1=None
         self.calllist2=None
+        #self.geoInfo()
         
     def update(self):
         self.Jm.update(self.rotx)
@@ -80,7 +81,7 @@ class link:
             glVertex3fv(v)
         glEnd()        
     
-    def geoinfo(self):  
+    def geoInfo(self):  
         _r=self.r
         self.calllist1=glGenLists(1)
         dcircle1=[(_r*np.cos(i), _r*np.sin(i), -_r/2.0) for i in np.linspace(0, 330, 10)*d2r]
@@ -121,31 +122,184 @@ class Robot:
             jl[0].update(thetas[i]) 
 #            print jl[0].Im.Rot
             jl[1].update()
+    
+    def draw(self):
+        for i in range(len(self.J_L)):
+            jl=self.J_L[i]
+            jl[1].draw()
+
 
 from wx import *
 
 class RobotDrawingBoard(glcanvas.GLCanvas):
-    def __init__(self, parent, robots):
+    def __init__(self, parent, robot_):
         glcanvas.GLCanvas.__init__(self, parent, -1, style=wx.WANTS_CHARS)
-        self.robots=robots
+        self.robot=robot_
+        self.minx=-5
+        self.maxx=5
+        self.miny=-5
+        self.maxy=5
+        self.minz=-5
+        self.maxz=5
+        self.keychar=None
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+        self.Bind(wx.EVT_MOUSEWHEEL, self.OnMouseWheel)
+        self.Bind(wx.EVT_KEY_DOWN, self.onKeyPress)
+        self.Bind(wx.EVT_KEY_UP, self.onKeyPressUp)
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.init=False
         
     def add_robot(self, robot_):
-        self.robots.append(robot_)
+        self.robot.append(robot_)
+
+    def InitGL(self):
+        self.ViewControl()
+        glMatrixMode(GL_MODELVIEW)
+        glRotate(-90, 1,0,0)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable (GL_COLOR_MATERIAL )
+        #glEnable(GL_NORMALIZE)
+        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, [0.8, 0.8, 0.8, 1.0])
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, [0.5, 0.5, 0.5, 1.0])
+        glLightfv(GL_LIGHT0, GL_POSITION, [0,0, 10])
+
+        self.axes=glGenLists(1)
+        glNewList(self.axes, GL_COMPILE)
+        glBegin(GL_LINES)
+        glColor3f(1.0, 1.0, 1.0)
+        glVertex3f(0,0,0)
+        glVertex3f(1,0,0)
+        glColor3f(0.0,1.0,0.0)
+        glVertex3f(0,0,0)
+        glVertex3f(0,1,0)
+        glColor3f(0.0, 0.0, 1.0)
+        glVertex3f(0,0,0)
+        glVertex3f(0,0,1)
+        glEnd() 
+        glEndList()
+        self.OnDraw()
+
+    def onKeyPressUp(self, evt):
+        self.keychar=None
+        evt.Skip()
+
+    def onKeyPress(self, evt):
+        keycode=evt.GetKeyCode()
+        if keycode<256:
+            char = chr(keycode).lower()
+            if char=='x' or char=='y' or char=='z': 
+                self.keychar=char
+        else:
+            char=keycode
+            if char==wx.WXK_UP or char==wx.WXK_DOWN or char==wx.WXK_LEFT or char==wx.WXK_RIGHT or char==wx.WXK_HOME:
+                self.keychar=char
+                self.ViewControl()  
+                self.Refresh()
+                self.Update()
+        evt.Skip()
+    
+    def OnMouseWheel(self, evt):    
+        rot=evt.GetWheelRotation()
+        self.pn=rot/120
+        dx=0.0
+        dy=0.0
+        if self.keychar==None:     
+            self.minx=self.minx-dx*self.pn
+            self.maxx=self.maxx+dx*self.pn
+            self.miny=self.miny-dy*self.pn
+            self.maxy=self.maxy+dy*self.pn
+        self.ViewControl()  
+        self.Refresh()
+        self.Update()
+        evt.Skip()
+
+    def ViewControl(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        disp=-0.1
+        if self.keychar==wx.WXK_UP:
+            self.miny=self.miny+disp
+            self.maxy=self.maxy+disp
+        elif self.keychar==wx.WXK_DOWN:  
+            self.miny=self.miny-disp
+            self.maxy=self.maxy-disp            
+        elif self.keychar==wx.WXK_LEFT:
+            self.minx=self.minx-disp
+            self.maxx=self.maxx-disp            
+        elif self.keychar==wx.WXK_RIGHT:
+            self.minx=self.minx+disp
+            self.maxx=self.maxx+disp                  
+        glOrtho(self.minx, self.maxx, self.miny, self.maxy, self.minz, self.maxz) 
+        glMatrixMode(GL_MODELVIEW)
+        if self.keychar == 'x' or self.keychar == 'y'  or self.keychar == 'z':            
+            prem=glGetFloat(GL_MODELVIEW_MATRIX)
+            glLoadIdentity()
+            delta=-5
+            if self.keychar=='x':
+                glRotate(delta*self.pn, 1,0,0)
+            elif  self.keychar=='y':  
+                glRotate(delta*self.pn, 0,1,0)
+            else:
+                glRotate(delta*self.pn, 0,0,1)
+            glMultMatrixf(prem)   
+        elif self.keychar==wx.WXK_HOME:
+            glLoadIdentity()
+
+    def OnPaint(self, evt):
+        self.SetCurrent()
+        if not self.init:
+            self.InitGL()
+            self.init = True
+        self.OnDraw()
+        self.SwapBuffers()
+        evt.Skip()
+
+    def OnDraw(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glCallList(self.axes)
+        self.robot.draw()
 #        
+    def OnSize(self, event):
+        wx.CallAfter(self.DoSetViewport)
+        event.Skip()
+
+    def DoSetViewport(self):
+        if self.IsShown():
+            self.SetCurrent()
+            size = self.size = self.GetClientSize()
+            glMatrixMode(GL_PROJECTION)
+            glViewport(0, 0, size.width, size.height)
+            glMatrixMode(GL_MODELVIEW)
+
 class MainWindow(wx.Frame):
     def __init__(self, parent = None,id = -1, title = "Syringa Robotics Inc."):        
         wx.Frame.__init__(
                 self, parent, id, title, size = (1000,800),
                 style = wx.DEFAULT_FRAME_STYLE #| wx.NO_FULL_REPAINT_ON_RESIZE
         )
-        self.robots=[]
-        self.glwin=RobotDrawingBoard(self, self.robots)
+        DHR=((1, 0, 90*d2r, 1*0.2), (0, 1.0, 0.0, 1*0.2), (0, 1.0, 0.0, 1*0.2))
+        self.robot=Robot(DHR)
+        self.glwin=RobotDrawingBoard(self, self.robot)
         box = wx.BoxSizer(wx.HORIZONTAL)
         box.Add(self.glwin, 1, wx.ALIGN_CENTRE|wx.ALL|wx.EXPAND, 5)
         self.SetSizer(box)    
+        self.SetAutoLayout(True)
+        self.Layout()
+        self.CreateStatusBar()
+        filemenu = wx.Menu()
+        menuitem = filemenu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
+        self.Bind(wx.EVT_MENU, self.OnExit, id=wx.ID_EXIT) # here comes the event-handler
         
+        menubar = wx.MenuBar()
+        menubar.Append(filemenu,"&File")
+        self.SetMenuBar(menubar)
         self.Show(True)
         
+    def OnExit(self,evt):
+        self.Close()
+        evt.Skip()
 #if __name__=="__main__":
 #    DHR=((1, 0, 90*d2r, 1*0.2), (0, 1.0, 0.0, 1*0.2), (0, 1.0, 0.0, 1*0.2))
 #    robot=Robot(DHR)
