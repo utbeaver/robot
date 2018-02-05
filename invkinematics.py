2538dd17454bae851a4a536f05dcc29609c4af58
utbeaver <peilin.song@gmail.com>
2018-02-05 00:10:08 -0500

    link geometry works

--- invkinematics.py
+++ invkinematics.py
@@ -1,29 +1,42 @@
 import numpy as np
 import matplotlib
 from OpenGL.GL import *
+from wx import *
+
 d2r=np.pi/180.0
 r2d=1.0/d2r
 
 
 
 class Marker:
-    def __init__(self, _pos=np.matrix([[0.0,0.0,0.0]]).T, _prev=None):
+    def __init__(self, _name="baseM", _pos=np.matrix([[0.0,0.0,0.0]]).T, _prev=None):
+        self.name=_name
         self.pos=_pos
         self.prevM=_prev
         self.Rot=np.matrix([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+        self.dh=np.matrix([[0.0, 0.0, 0.0, 0.0], 
+                           [0.0, 0.0, 0.0, 0.0], 
+                           [0.0, 0.0, 0.0, 0.0], 
+                           [0.0, 0.0, 0.0, 1.0]])
         self.P=self.pos
 
     def update(self, _m):
         if self.prevM!=None:
             self.Rot=self.prevM.Rot*_m
-            self.P=self.Rot*self.pos+self.prevM.P
+            self.P=self.prevM.Rot*self.pos+self.prevM.P
+            self.dh[:3, :3]=self.Rot
+            self.dh[:3, 3]=self.P
+
+    def DH(self):
+        return self.dh
   
         
 
 class Joint:
-    def __init__(self, _J, val=0):
+    def __init__(self, _name, _J, val=0):
+        self.name=_name
         self.Jm=_J
-        self.Im=Marker(np.matrix([[0.0,0.0,0.0]]).T, self.Jm)
+        self.Im=Marker(self.name+"Im", np.matrix([[0.0,0.0,0.0]]).T, self.Jm)
         self.val=val
         self.rotz=np.matrix([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]) 
 
@@ -43,9 +56,10 @@ class Joint:
 
 
 class link:
-    def __init__(self, paras, I=None):
+    def __init__(self, _name, paras, I=None):
+        self.name=_name
         self.m=1.0
-        self.scale=0.5
+        self.scale=0.5#
         self.Im=I
         self.d=paras[0]
         self.a=paras[1]
@@ -54,64 +68,76 @@ class link:
         c=np.cos(self.alpha)
         s=np.sin(self.alpha)
         self.rotx=np.matrix([[1.0, 0.0, 0.0], [0.0, c, -s], [0.0, s, c]]) 
-        self.Jm=Marker(np.matrix([[self.a, 0.0, self.d]]).T, self.Im)
-        self.cm=Marker(np.matrix([[self.a*self.scale, 0, self.d*self.scale]]).T, self.Im)
+        self.Jm=Marker(self.name+"_"+"Jm", np.matrix([[self.a, 0.0, self.d]]).T, self.Im)
+        #self.cm=Marker(np.matrix([[self.a*self.scale, 0, self.d*self.scale]]).T, self.Im)
         self.update()
         self.calllist1=None
         self.calllist2=None
-        #self.geoInfo()
+        self.init=False
+#        self.geoInfo()
         
     def update(self):
         self.Jm.update(self.rotx)
-#        print self.rotx
-        self.cm.update(self.rotx)
+        #self.cm.update(self.rotx)
     
-    def twoCyn(dc1, ac1):
-        glBegin(GL.GL_LINES)
-        for v in dc1: 
-            glVertex3fv(v)
-            glVertex3fv((v[0], v[1], ))
+    def twoCyn(self, c1, c2):
+        glBegin(GL_LINES)
+        for i in range(len(c1)): 
+            v1=c1[i]
+            glVertex3fv(v1)
+            v2=c2[i]
+            glVertex3fv((v2))
         glEnd()
-        glBegin(GL.GL_LINE_LOOP)
-        for v in dcircle1:
-            glVertex3fv(v)
+        glBegin(GL_LINE_LOOP)
+        for v in c1: glVertex3fv(v)
         glEnd()
-        glBegin(GL.GL_LINE_LOOP)
-        for v in ac1:
-            glVertex3fv(v)
+        glBegin(GL_LINE_LOOP)
+        for v in c2: glVertex3fv(v)
         glEnd()        
     
     def geoInfo(self):  
         _r=self.r
+        scale=1.5
+        if self.d>0.0: scale=1.0
         self.calllist1=glGenLists(1)
-        dcircle1=[(_r*np.cos(i), _r*np.sin(i), -_r/2.0) for i in np.linspace(0, 330, 10)*d2r]
-        dcircle2=[(vs[0], vs[1], vs[2]+self.d+_r) for vs in dcircle1]
-        glNewList(self.calllist1, GL.GL_COMPILE)
+        dcircle1=[(_r*np.cos(i), _r*np.sin(i), -_r*scale) for i in np.linspace(0, 330, 10)*d2r]
+        dcircle2=[(vs[0], vs[1], self.d+_r*scale) for vs in dcircle1]
+        glNewList(self.calllist1, GL_COMPILE)
         self.twoCyn(dcircle1, dcircle2)
-        glEndlist()
+        glEndList()
         if self.a>0.0:
             self.calllist2=glGenLists(1)
             acircle1=[(0.0, _r*np.cos(i), _r*np.sin(i)+self.d) for i in np.linspace(0, 330, 10)*d2r]
             acircle2=[(self.a, vs[1], vs[2]) for vs in acircle1]
-            glNewList(self.calllist2, GL.GL_COMPILE)
+            glNewList(self.calllist2, GL_COMPILE)
             self.twoCyn(acircle1, acircle2)
-            glEndlist()
+            glEndList()
             
     def draw(self):
+        m=self.Im
+        #print m.name
+        #print m.DH()
+        if not self.init:
+            self.geoInfo()
+            self.init=True
+        glPushMatrix()
+        glMultMatrixf(self.Im.DH().flatten('F'))
         if self.calllist1 != None:
             glCallList(self.calllist1)
         if self.calllist2 != None:
             glCallList(self.calllist2)            
+        glPopMatrix()
 
-        
 class Robot:
     def __init__(self, DHR):
         self.baseM=Marker()
         J=self.baseM
         self.J_L=[]
+        i=0
         for dhr_ in DHR:
-            I=Joint(J)
-            lnk=link(dhr_, I.IM())
+            i=i+1
+            I=Joint("J"+str(i), J)
+            lnk=link("lnk"+str(i), dhr_, I.IM())
             J=lnk.Jm
             self.J_L.append((I, lnk))
 
@@ -119,17 +145,13 @@ class Robot:
     def forwardK(self, thetas):
         for i in range(len(self.J_L)):
             jl=self.J_L[i]
-            jl[0].update(thetas[i]) 
-#            print jl[0].Im.Rot
-            jl[1].update()
+            jl[0].update(thetas[i]) #joint 
+            jl[1].update()  #link
     
     def draw(self):
         for i in range(len(self.J_L)):
             jl=self.J_L[i]
             jl[1].draw()
-
-
-from wx import *
 
 class RobotDrawingBoard(glcanvas.GLCanvas):
     def __init__(self, parent, robot_):
@@ -176,10 +198,10 @@ class RobotDrawingBoard(glcanvas.GLCanvas):
         glVertex3f(0,1,0)
         glColor3f(0.0, 0.0, 1.0)
         glVertex3f(0,0,0)
-        glVertex3f(0,0,1)
+        glVertex3fv((0,0,1))
         glEnd() 
         glEndList()
-        self.OnDraw()
+        #self.OnDraw()
 
     def onKeyPressUp(self, evt):
         self.keychar=None
@@ -281,8 +303,9 @@ class MainWindow(wx.Frame):
                 self, parent, id, title, size = (1000,800),
                 style = wx.DEFAULT_FRAME_STYLE #| wx.NO_FULL_REPAINT_ON_RESIZE
         )
-        DHR=((1, 0, 90*d2r, 1*0.2), (0, 1.0, 0.0, 1*0.2), (0, 1.0, 0.0, 1*0.2))
+        DHR=((1.0, 0.0, 90*d2r, 1*0.2), (0.0, 1.0, 0.0, 1*0.2))#, (0, 1.0, 0.0, 1*0.2))
         self.robot=Robot(DHR)
+        self.robot.forwardK([0,0])
         self.glwin=RobotDrawingBoard(self, self.robot)
         box = wx.BoxSizer(wx.HORIZONTAL)
         box.Add(self.glwin, 1, wx.ALIGN_CENTRE|wx.ALL|wx.EXPAND, 5)
